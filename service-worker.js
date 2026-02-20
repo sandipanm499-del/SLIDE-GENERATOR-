6604cf6fdf625d57c83041f2ca3f666f0553cb8a
sandipanm499-del <sandipanm499@gmail.com>
2026-02-20 09:21:50 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "mcq-pwa-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "style.css",
+        "app.js",
+        "manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
